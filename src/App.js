c1cee04c4b9e86c4f6ac1ea28876a62f3906712e
aibek <holbinaiba@gmail.com>
2023-12-20 17:29:37 +0600

    add song-player

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ import Player from "./components/Player/Player";
 function App() {
   // states
   const [songs, setSongs] = useState([]);
-  const [currentSong, setCurrentSong] = useState([0]);
+  const [currentSong, setCurrentSong] = useState({});
   const [isPlaying, setIsPlaying] = useState(false);
   const audioRef = useRef(null);
   const [songInfo, setSongInfo] = useState({
@@ -22,67 +22,85 @@ function App() {
     try {
       const response = await getTracks(songs);
       setSongs(response.data);
+      setCurrentSong(response.data[0]);
     } catch (err) {
       console.log(err.response);
     }
   };
-
+  console.log(songInfo);
   useEffect(() => {
     getData();
   }, []);
 
   // timeUpdateHandler
-
   const timeUpdateHandler = (e) => {
     const current = e.target.currentTime;
     const duration = e.target.duration;
-
+    //calculating percentage
     const roundedCurrent = Math.round(current);
     const roundedDuration = Math.round(duration);
-    const amimation = Math.round((roundedCurrent / roundedDuration) * 100);
-
+    const animation = Math.round((roundedCurrent / roundedDuration) * 100);
+    console.log();
     setSongInfo({
       currentTime: current,
-      duration: duration,
-      animationPercentage: amimation,
+      duration,
+      animationPercentage: animation,
     });
   };
-
   // songEndHandler
-
-  const songEndHandler = async (e) => {
+  const songEndHandler = async () => {
     let currentIndex = songs.findIndex((song) => song.id === currentSong.id);
-
     await setCurrentSong(songs[(currentIndex + 1) % songs.length]);
-    if (isPlaying) audioRef.current.play();
+
+    // await setCurrentSong(songs[(currentIndex + 1) % songs.length]);
+
+    // if (isPlaying) audioRef.current.play();
+    audioRef.current.addEventListener('canplaythrough', async () => {
+  
+      if (isPlaying) audioRef.current.play();
+    }, { once: true });
   };
-  console.log(currentSong);
+  
+
+  console.log(audioRef.current);
+
   return (
     <div className={styles.wrapper}>
       <div className={styles.music_panel}>
         <div className={styles.music_section_1}>
-          <Main 
-           setCurrentSong={setCurrentSong}
-           songs={songs}
-           setSongs={setSongs}
-           isPlaying={isPlaying}
-           audioRef={audioRef}
+          <Main
+            setCurrentSong={setCurrentSong}
+            songs={songs}
+            setSongs={setSongs}
+            isPlaying={isPlaying}
+            audioRef={audioRef}
           />
         </div>
         <div className={styles.music_section_2}>
           <SideBar />
         </div>
       </div>
-        <div className={styles.music_section_3}>
-          <Player currentSong={currentSong}/>
-        </div>
-        <audio
-          onLoadedMetadata={timeUpdateHandler}
-          onTimeUpdate={timeUpdateHandler}
-          src={currentSong?.url}
-          ref={audioRef}
-          onEnded={songEndHandler}
-        ></audio>
+      <div className={styles.music_section_3}>
+        <Player
+          id={songs.id} 
+          songs={songs} 
+          songInfo={songInfo} 
+          setSongInfo={setSongInfo} 
+          audioRef={audioRef} 
+          isPlaying={isPlaying} 
+          setIsPlaying={setIsPlaying} 
+          currentSong={currentSong} 
+          setCurrentSong={setCurrentSong} 
+          setSongs={setSongs} 
+        />
+      </div>
+      <audio
+        onLoadedMetadata={timeUpdateHandler}
+        onTimeUpdate={timeUpdateHandler}
+        src={currentSong.url}
+        ref={audioRef}
+        onEnded={songEndHandler}
+      ></audio>
     </div>
   );
 }
--- src/components/Player/Player.jsx
+++ src/components/Player/Player.jsx
@@ -9,37 +9,136 @@ import {
   faVolumeUp,
   faVolumeDown,
 } from "@fortawesome/free-solid-svg-icons";
+import { faVolumeMute } from "@fortawesome/free-solid-svg-icons";
 
-const Player = ({ currentSong }) => {
-  const song = currentSong[0];
+const Player = ({
+  currentSong,
+  isPlaying,
+  setIsPlaying,
+  audioRef,
+  setSongInfo,
+  songInfo,
+  songs,
+  setCurrentSong,
+  id,
+  setSongs,
+}) => {
+  // functinos
+  const handleTogglePlay = () => {
+    console.log("Hello!");
+    const prevValue = isPlaying;
+    setIsPlaying(!prevValue);
+    if (!prevValue) {
+      audioRef.current.play();
+    } else {
+      audioRef.current.pause();
+    }
+  };
+  const activeLibraryHandler = (nextPrev) => {
+    const newSongs = songs.map((song) => {
+      if (song.id === nextPrev.id) {
+        return {
+          ...song,
+          active: true,
+        };
+      } else {
+        return {
+          ...song,
+          active: false,
+        };
+      }
+    });
+    setSongs(newSongs);
+    console.log("Hey from useEffect form player JS");
+  };
+  //Event Handlers
+  const dragHandler = (e) => {
+    audioRef.current.currentTime = e.target.value;
+    setSongInfo({ ...songInfo, currentTime: e.target.value });
+  };
+
+  const getTime = (time) =>
+    Math.floor(time / 60) + ":" + ("0" + Math.floor(time % 60)).slice(-2);
+  const skipTrackHandler = async (direction) => {
+    let currentIndex = songs.findIndex((song) => song.id === currentSong.id);
+    if (direction === "skip-forward") {
+      await setCurrentSong(songs[(currentIndex + 1) % songs.length]);
+      activeLibraryHandler(songs[(currentIndex + 1) % songs.length]);
+    }
+    if (direction === "skip-back") {
+      if ((currentIndex - 1) % songs.length === -1) {
+        await setCurrentSong(songs[songs.length - 1]);
+        // playAudio(isPlaying, audioRef);
+        activeLibraryHandler(songs[songs.length - 1]);
+
+        return;
+      }
+      await setCurrentSong(songs[(currentIndex - 1) % songs.length]);
+      activeLibraryHandler(songs[(currentIndex - 1) % songs.length]);
+    }
+    if (isPlaying) audioRef.current.play();
+  };
+  //adding the styles
+
+  const handleToggleSound = () => {
+    const audio = audioRef.current;
+
+    if (audio.muted) {
+      audio.muted = false;
+    } else {
+      audio.muted = true;
+    }
+  };
+
   return (
     <div className={styles.player}>
       <div className={styles.section_1}>
-        <img src={song?.artwork} className={styles.content_img} alt="" />
+        <img src={currentSong.artwork} className={styles.content_img} alt="" />
         <div className={styles.content}>
-          <h3 className={styles.content_title}>{song?.title}</h3>
-          <p className={styles.content_text}>{song?.artist}</p>
+          <h3 className={styles.content_title}>{currentSong.title}</h3>
+          <p className={styles.content_text}>{currentSong.artist}</p>
         </div>
       </div>
       <div className={styles.section_2}>
         <div className={styles.range}>
-          <p className={styles.range_time}>00:00</p>
+          <p className={styles.range_time}>{getTime(songInfo.currentTime)}</p>
           <input
-              type="range"
-              min={0}
-              max={100}
-              className={styles.range_track}
-            />
-          <p className={styles.range_time}>03:09</p>
+            type="range"
+            onChange={dragHandler}
+            value={songInfo.currentTime}
+            min={0}
+            max={songInfo.duration || 0}
+            className={styles.range_track}
+          />
+          <p className={styles.range_time}>
+            {songInfo.duration ? getTime(songInfo.duration) : "00:00"}
+          </p>
         </div>
         <div className={styles.controld}>
-          <FontAwesomeIcon icon={faAngleLeft} className={styles.icon}/>
-          <FontAwesomeIcon icon={faPlay} className={styles.icon} />
-          <FontAwesomeIcon icon={faAngleRight} className={styles.icon}/>
+          <FontAwesomeIcon
+            icon={faAngleLeft}
+            className={styles.icon}
+            onClick={() => skipTrackHandler("skip-back")}
+          />
+          <FontAwesomeIcon
+            icon={!isPlaying ? faPlay : faPause}
+            className={styles.icon}
+            onClick={handleTogglePlay}
+          />
+          <FontAwesomeIcon
+            icon={faAngleRight}
+            className={styles.icon}
+            onClick={() => skipTrackHandler("skip-forward")}
+          />
         </div>
       </div>
       <div className={styles.section_3}>
-        <FontAwesomeIcon size="2px" icon={faVolumeUp} className={styles.icon}/>
+        <FontAwesomeIcon
+          size="2px"
+          icon={!audioRef.current.muted ? faVolumeUp : faVolumeMute}
+          className={styles.icon}
+          onClick={handleToggleSound}
+        />
       </div>
     </div>
   );
